0ae4744f8e6762022b44be0502ffda60099df4a0
Anderson Inacio <anderson.mouratoo@gmail.com>
2024-02-24 10:06:26 -0300

    update

--- Módulo_01/src/desafio03/ArrayFarmacia.java
+++ Módulo_01/src/desafio03/ArrayFarmacia.java
@@ -11,10 +11,10 @@ public class ArrayFarmacia {
 		Scanner scan2 = new Scanner (System.in);
 		List<String> medicamentos = new ArrayList<>();
 		List<Integer> estoque = new ArrayList<>();
-		Boolean op = true;
-		int resposta = 0;
+		Boolean loop = true;
+		int op = 0;
 		
-		while (op) {
+		while (loop) {
 			System.out.println();
 			System.out.println("#################### MENU ####################");
 			System.out.println("[1] - ADICIONAR MEDICAMENTO");
@@ -23,21 +23,21 @@ public class ArrayFarmacia {
 			System.out.println("[4] - ENCERRAR PROGRAMA");
 			System.out.println("______________________________________________");
 			System.out.print("Selecione uma opção: ");
-			resposta = scan.nextInt();
+			op = scan.nextInt();
 			
-			if(resposta == 1) {
+			if(op == 1) {
 				System.out.println("INFORME O NOME DO MEDICAMENTO: ");
 				medicamentos.add(medicamentos.size(), scan2.nextLine());
 				System.out.print("INFORME A QUANTIDADE DE ESTOQUE: ");
 				estoque.add(estoque.size(), scan.nextInt());
 				
-			}else if(resposta == 2) {
+			}else if(op == 2) {
 				for(int i=0; i< medicamentos.size(); i++) {
 					System.out.println("__________________________________________");
 					System.out.println("NOME: " + medicamentos.get(i));
 					System.out.println("ESTOQUE: " + estoque.get(i));
 				}				
-			}else if(resposta == 3){
+			}else if(op == 3){
 				System.out.println("Digite o nome para pesquisar: ");
 				String seach = scan2.nextLine();
 				if(seach != "") {
@@ -50,9 +50,9 @@ public class ArrayFarmacia {
 				}else {
 					System.err.println("Invalido");
 				}
-			}else if(resposta == 4) {
+			}else if(op == 4) {
 				System.out.println("Programa finalizado com exito!");
-				op = false;
+				loop = false;
 			}else {
 				System.out.println("Opção invalida, tente novamente");
 			}
